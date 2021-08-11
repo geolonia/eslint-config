dc209ce6b01d97ec8265b304ee1827288f0b73ba
kamataryo <kamataryo@users.noreply.github.com>
2021-08-11 18:46:31 +0900

    Update

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  env: {
-    es6: true,
-    node: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
   rules: {
     'arrow-parens': 'error',
     'arrow-spacing': 'error',
