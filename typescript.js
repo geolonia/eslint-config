5b51345924bbc592429e0bd8e0dc5dc80ab08b04
kamataryo <kamataryo@users.noreply.github.com>
2021-09-01 10:14:49 +0900

    Fix coding format

--- typescript.js
+++ typescript.js
@@ -11,7 +11,20 @@ module.exports = {
     // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
     '@typescript-eslint/no-misused-promises': [ 'error', { 'checksConditionals': true }],
 
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-extra-semi': 'off',
+
+    // NOTE: rule conflictions
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#extension-rules'
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': config.rules['no-unused-vars'],
+    '@typescript-eslint/no-unused-vars': config.rules['no-unused-vars'] || 'warn',
   },
 };
