1c123e07d32fb6e878723cbbebc923203e821b00
kamataryo <kamataryo@users.noreply.github.com>
2021-09-15 17:16:16 +0900

    Supress 'import/no-extraneous-dependencies' for test files, because they sometimes import devDeps

--- index.js
+++ index.js
@@ -46,4 +46,20 @@ module.exports = {
 
     'import/no-extraneous-dependencies': ['warn', { 'devDependencies': false }],
   },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+        '**/*.test.ts',
+        '**/*.test.jsx',
+        '**/*.test.tsx',
+        '**/*.spec.js',
+        '**/*.spec.ts',
+        '**/*.spec.jsx',
+        '**/*.spec.tsx',      ],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
 };
