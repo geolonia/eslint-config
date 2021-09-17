434cf9eb99b2e3ffd3a2c18d70c879928770a4e7
kamataryo <kamataryo@users.noreply.github.com>
2021-09-17 13:44:33 +0900

    Moderate a rule

--- typescript.js
+++ typescript.js
@@ -11,9 +11,11 @@ module.exports = {
   ],
   rules: {
     ...config.rules,
+
     // NOTE: Some @typescript-eslint/eslint-plugin rules requires type information.
     // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
-    '@typescript-eslint/no-misused-promises': [ 'error', { 'checksConditionals': true }],
+
+    '@typescript-eslint/no-misused-promises': [ 'warn', { 'checksConditionals': true }],
 
     '@typescript-eslint/ban-ts-comment': ['warn', { 'ts-ignore': true }],
     '@typescript-eslint/ban-types': 'off',
